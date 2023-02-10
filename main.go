c16ddb2ee1b00657171e35f443b1dfe2a0a622e6
xieyuhua <1130107545@qq.com>
2023-02-10 09:42:16 +0800

    fix bug init

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"net/http"
-
+    "fmt"
+    "log"
 	"github.com/gobuffalo/packr/v2"
 )
 
 func main() {
 	box := packr.New("someBoxName", "./public")
+
+    // Get the string representation of a file, or an error if it doesn't exist:
+    // s, err := box.FindString("index.html")
+    
+    // Get the []byte representation of a file, or an error if it doesn't exist:
+    s, err := box.Find("index.html")
+    
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(s)
+
 
 	http.Handle("/", http.FileServer(box))
 	http.ListenAndServe(":3000", nil)
